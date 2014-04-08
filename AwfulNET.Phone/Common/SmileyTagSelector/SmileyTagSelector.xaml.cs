07b9aba3b772b9e304aec68d2ebb64f6f703e406
Nathan Simpson <nathanisaac@gmail.com>
2014-04-07 20:10:54 -0500

    Updated SmileyTagSelector.

--- AwfulNET.Phone/Common/SmileyTagSelector/SmileyTagSelector.xaml.cs
+++ AwfulNET.Phone/Common/SmileyTagSelector/SmileyTagSelector.xaml.cs
@@ -7,14 +7,57 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using AwfulNET.Common;
+using AwfulNET.DataModel;
+using System.Windows.Input;
 
-namespace AwfulNET.Phone.Common.TagSmileyView
+namespace AwfulNET.Phone.Common
 {
     public partial class SmileyTagSelector : UserControl
     {
+        public event EventHandler TagSelected;
+
+        private SmileyTagSelectorViewModel context;
         public SmileyTagSelector()
         {
             InitializeComponent();
+            this.MainPivot.SelectionChanged += MainPivot_SelectionChanged;
+            context = this.SelectorViewModel;
+            context.SelectTagCommand = new RelayCommand<object>(SelectTag);
+            this.DataContext = context;
         }
+
+        private async void MainPivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var pivot = sender as Pivot;
+            var selected = pivot.SelectedItem as PivotItem;
+            if (selected != null && selected.DataContext is SmileyTagGroup)
+                await context.Smilies.OnSelectedAsync(this, IProgressFactory.GenerateProgressToken());
+        }
+
+        private void SelectTag(object value)
+        {
+            CodeTagDataModel item = value as CodeTagDataModel;
+            if (value != null)
+            {
+                // copy code to clipboard
+                Clipboard.SetText(item.Code);
+
+                // notify event listeners that we selected an item
+                var handler = TagSelected;
+                if (handler != null)
+                    TagSelected(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public sealed class SmileyTagSelectorViewModel : BindableBase
+    {
+        private static readonly SmileyTagGroup smilies = new SmileyTagGroup();
+        public SmileyTagGroup Smilies { get { return smilies; } }
+
+        public ICommand SelectTagCommand { get; set; }
+
+        public string Title { get { return "select to copy code"; } }
     }
 }
